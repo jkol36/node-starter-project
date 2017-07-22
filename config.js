89244ca9d6e369ecabc6e47999ab94902de52e6a
Jon Kolman <jonathankolman@gmail.com>
2017-07-22 08:45:06 -0400

    remove all sales every 23 hours and 55 minutes

--- config.js
+++ config.js
@@ -1,1 +1,14 @@
+import firebaseAdmin from 'firebase-admin'
 global.Promise = require('bluebird')
+
+const serviceAccount = require('./serviceAccount.json')
+
+export const firebase = firebaseAdmin.initializeApp({
+  credential: firebaseAdmin.credential.cert(serviceAccount),
+  databaseURL: "https://ticketlynx-5a17f.firebaseio.com"
+
+})
+
+export const onSaleRef = firebase.database().ref('onSale')
+//create a firebase ref that serves as a signaling tool for the scrapers to run
+export const signalingRef = firebase.database().ref('signalingRef')
