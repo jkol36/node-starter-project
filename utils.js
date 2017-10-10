b79ed6a97bb5498dd40956c83ab2c9ced035fd31
Jonathan Kolman <jonathankolman@Jonathans-MacBook-Pro.local>
2017-10-10 01:22:23 -0400

    added common utils to node-starter-project

---
+++ utils.js
@@ -0,0 +1,17 @@
+export const eliminateDuplicates = (array) => {
+  let obj = {}
+  let out = []
+  for(let i=0; i<array.length; i++) {
+    obj[array[i]] = 0
+  }
+  for (let x in obj) {
+    out.push(x)
+  }
+  return out
+}
+
+export const calculatePercentageDone = (array, item) => {
+  let x = array.indexOf(item)
+  let y = x+1
+  return y/array.length
+}
