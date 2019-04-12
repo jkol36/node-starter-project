5b29c837885c58eb388c99c1a2bee20ddd82f573
jon <jon@jons-MacBook-Pro.local>
2019-04-11 21:13:35 -0400

    use async/await

--- app.js
+++ app.js
@@ -2,15 +2,3 @@ async function f () {
 	return 'async function works'
 }
 
-
-const testPromise = () => {
-	return new Promise((resolve, reject) => {
-		resolve('promises running')
-	})
-}
-
-f()
-.then(console.log)
-
-testPromise()
-.then(console.log)
